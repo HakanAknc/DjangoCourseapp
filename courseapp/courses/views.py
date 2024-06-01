6cb31f2e5b66722c2deea527698c5e044135d600
Hakan Akinci <hakan062.2001@gmail.com>
2024-06-01 13:21:12 +0300

    Templates & Static Files

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
@@ -8,22 +9,56 @@ data = {
     "mobil":"mobil kategorisine ait kurslar",
 }
 
-# http://127.0.0.1:8000/kurslar
+db = {
+    "courses" : [
+        {
+            "title":"javascript kursu",
+            "description":"javascript kurs açıklaması",
+            "imageUrl":"https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+            "slug":"javascript-kursu",
+            "date":date(2024,10,10),
+            "isActive":True,
+            "isUpdated":False
+        },
+        {
+            "title":"python kursu",
+            "description":"python kurs açıklaması",
+            "imageUrl":"https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+            "slug":"python-kursu",
+            "date":date(2024,9,10),
+            "isActive":False,
+            "isUpdated":False
+        },
+        {
+            "title":"web geliştirme kursu",
+            "description":"web geliştirme kurs açıklaması",
+            "imageUrl":"https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+            "slug":"web-gelistirme-kursu",
+            "date":date(2024,8,10),
+            "isActive":True,
+            "isUpdated":True
+        }
+    ],
+    "categories": [
+            { "id":1, "name": "programlama", "slug":"programlama" },
+            { "id":2, "name": "web geliştirme", "slug":"web-gelistirme" },
+            { "id":3, "name": "mobil uygulamalar", "slug":"mobil-uygulamlar" },
+        ]
+}
 
 def index(request):
-    return render(request, 'courses/index.html')
+    # list comphension
+    kurslar = [course for course in db["courses"] if course["isActive"]==True]
+    kategoriler = db["categories"]
 
-def kurslar(request):
-    list_items = ""
-    category_list = list(data.keys())
+    # for kurs in db["courses"]:
+    #     if kurs["isActive"] == True:
+    #         kurslar.append(kurs)
 
-    for category in category_list:
-        redirect_url = reverse('courses_by_category', args=[category])
-        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
-
-    html = f"<h1>kurs listesi</h1><br><ul>{list_items}</ul>"
-
-    return HttpResponse(html)
+    return render(request, 'courses/index.html', {
+        'categories' : kategoriler,
+        'courses' : kurslar
+    })
 
 def details(requst, kurs_adi):
     return HttpResponse(f'{kurs_adi} detay sayfası')
@@ -31,7 +66,10 @@ def details(requst, kurs_adi):
 def getCoursesByCategory(request, category_name):
     try:
         category_text = data[category_name]
-        return HttpResponse(category_text)
+        return render(request, 'courses/kurslar.html', {
+            'category' : category_name,
+            'category_text' : category_text
+        })
     except:
         return HttpResponseNotFound("<h1>yanlış kategori seçimi</h1>")
 
